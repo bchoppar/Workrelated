1d08fef9d21196b96c424528aff9f9c523b486ea
bchoppar <bchoppar@gmail.com>
2024-08-26 16:54:15 +0530

    Getting O/P for required users

--- Angular-app/src/app/app.component.ts
+++ Angular-app/src/app/app.component.ts
@@ -3,17 +3,18 @@ import { UserComponent } from '../../src/user/user.component'
 import { HeaderComponent } from './header.component';
 import { TasksComponent } from '../../src/tasks/tasks.component'
 import { DUMMY_USERS } from './dummy-users';
+import { NgFor } from '@angular/common';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [HeaderComponent, UserComponent, TasksComponent],
+  imports: [HeaderComponent, UserComponent, NgFor, TasksComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
   users = DUMMY_USERS;
-  selectedUserId = "u1";
+  selectedUserId?: string;
 
   get selectedUser() {
     return this.users.find((user) => user.id === this.selectedUserId)!
